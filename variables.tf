004d3b0378eb7765e8a81519e27df2097abe132a
DanielLippross <Dlippross@spartaglobal.com>
2020-01-23 10:41:51 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  default = "vpc-0541bf4f245b805e1"
+}
+variable "name" {
+  default = "daniel-app-"
+}
+variable "db-name" {
+  default = "daniel-db-"
+}
+variable "app_ami_id" {
+  default = "ami-01ff995a60d0dba85"
+}
+variable "db_ami_id" {
+  default = "ami-0c1c20912b942fb91"
+}
